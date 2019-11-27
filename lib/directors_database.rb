e53ea5640513010fef0c7ea653c2d5f9b3f3f5bf
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-11-27 11:18:32 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -3,5 +3,13 @@ require 'yaml'
 def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
-  
+  total_gross = 0
+  row_index = 0
+  while row_index < directors_database.length do
+    column_index = 0
+    while column_index < directors_database[row_index].length do
+      inner_len = directors_database[row_index]
+      [column_index].length
+      inner_index = 0
+      while inner_index < inner_len do
 end
